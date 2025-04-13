3aa83c7b1524b393c879ebb3c5a28f8b931741dc
xiboliya-2007 <863642761@qq.com>
2025-04-13 16:16:24 +0800

    修改网站名字

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '刘垚利',
+  tagline: '我的网站',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
