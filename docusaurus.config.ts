6bc32eebf4af045c9aae62472b68c69610b19e02
xiboliya-2007 <863642761@qq.com>
2025-04-13 16:12:16 +0800

    init

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'xiboliya-2007', // Usually your GitHub org/user name.
-  projectName: 'yaolo-blog', // Usually your repo name.
+  projectName: 'yaoli-blog', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
